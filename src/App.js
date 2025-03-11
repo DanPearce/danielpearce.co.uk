061b02b8e4f71ab0a278c1c4265faad2ba2dacd3
DanPearce <daniel_pearce@outlook.com>
2025-03-11 12:33:10 +0000

    Add routes for all pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import NavBar from './components/NavBar';
 import styles from './styles/App.module.css';
+import Container from 'react-bootstrap/Container';
+import NavBar from './components/NavBar';
+import { Routes, Route} from 'react-router-dom';
+import LandingPage from './pages/LandingPage';
+import Projects from './pages/projects/Projects';
+import AboutMe from './pages/AboutMe';
+import Contact from './pages/Contact';
+import Resume from './pages/Resume';
 
 function App() {
   return (
     <div className={styles.App}>
       <NavBar />
+        <Container>
+          <Routes>
+            <Route path='/' element={<LandingPage />} />
+            <Route path='/projects' element={<Projects />} />
+            <Route path='/aboutme' element={<AboutMe />} />
+            <Route path='/contact' element={<Contact />} />
+            <Route path='/resume' element={<Resume />} />
+          </Routes>
+        </Container>
     </div>
   );
 }
